d4dce91f84c97814b8b3794bc4b2236daedb41a8
Sergey <fzastahov@gmail.com>
2022-09-11 13:24:19 +0300

    multifile structure

--- src/trainerV2/default/dep/test1.o.d
+++ src/trainerV2/default/dep/test1.o.d
@@ -1,4 +1,4 @@
-lab7.o: ../lab7.c c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+test1.o: ../test1.c c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
   c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
   c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
   c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
@@ -7,7 +7,7 @@ lab7.o: ../lab7.c c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
   c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
   c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
   c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h ../test1.h
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h
 
 c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
 
@@ -28,5 +28,3 @@ c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
 c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
 
 c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
-
-../test1.h:
---
+++ src/trainerV2/test1.h
@@ -0,0 +1,6 @@
+#ifndef TEST1_INCLUDE
+#define TEST1_INCLUDE
+
+int test1_func(void);
+
+#endif
