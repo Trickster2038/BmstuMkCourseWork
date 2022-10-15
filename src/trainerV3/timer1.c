16b68eb8cb26ac6e99c8a765fe8f01a52bb0dbab
Sergey <fzastahov@gmail.com>
2022-10-15 14:03:55 +0300

    fixes

--- src/trainerV3/timer1.c
+++ src/trainerV3/timer1.c
@@ -2,7 +2,7 @@
 
 void timer1_init(){
 	TCNT1=0x00; // ticks
-	TCCR1B |= (1<<CS10); // K = 0
+	TCCR1B |= (1<<CS10); // K = 1
 	OCR1A = (unsigned int) (F_CPU / 1000); // compare num
 	TIMSK |= (1<<OCIE1A); // launch timer	
 }
